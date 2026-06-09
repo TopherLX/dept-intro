6025b6dd18abb8f3c67de2f2f480734dffa8bbe9
Lixian Zhang <li.xian.zhang@outlook.com>
2026-06-09 15:20:50 +0800

    ci: add GitHub Pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import Components from 'unplugin-vue-components/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/dept-intro/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
